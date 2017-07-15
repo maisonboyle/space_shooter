3cb3d7128a9c3b650418711777f13573c239eadd
Douglas <maisonboyle01@gmail.com>
2017-07-15 23:22:16 +0100

    Initial. Audio added

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter (Collider other){
+		if (other.tag == "Boundary"){
+			return;
+			}
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Instantiate (explosion, transform.position, transform.rotation);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+
+}
