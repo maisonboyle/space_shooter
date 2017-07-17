43988385daa885dad163e8be4aebf8a5c9e2abcd
Douglas <maisonboyle01@gmail.com>
2017-07-17 15:33:23 +0100

    Touchpad mobile

---
+++ Assets/Scripts/SimpleTouchAreaButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SimpleTouchAreaButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler  {
+
+	private bool touched;
+	private int pointerID;
+	private bool canFire;
+
+	void Awake(){
+		touched = false;
+		canFire = false;
+	}
+
+
+	public void OnPointerDown (PointerEventData data){
+		// set start point
+		if (!touched) {
+			touched = true;
+			canFire = true;
+			pointerID = data.pointerId;
+		}
+	}
+
+	public void OnPointerUp (PointerEventData data){
+		// reset everything
+		if (data.pointerId == pointerID) {
+			touched = false;
+			canFire = false;
+		}
+	}
+
+	public bool CanFire(){
+		return canFire;
+	}
+
+}
---
+++ Assets/Scripts/SimpleTouchPad.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SimpleTouchPad : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler {
+
+	public float smoothing;
+
+	private Vector2 smoothDirection;
+	private Vector2 origin;
+	private Vector2 direction;
+	private bool touched;
+	private int pointerID;
+
+	void Awake(){
+		direction = Vector2.zero;
+		touched = false;
+	}
+
+
+	public void OnPointerDown (PointerEventData data){
+		// set start point
+		if (!touched) {
+			touched = true;
+			pointerID = data.pointerId;
+			origin = data.position;
+		}
+	}
+
+	public void OnDrag (PointerEventData data){
+		// Compare difference between start and current position
+		if (data.pointerId == pointerID) {
+			Vector2 currentPosition = data.position;
+			Vector2 directionRaw = currentPosition - origin;
+			direction = directionRaw.normalized;
+		}
+	}
+
+	public void OnPointerUp (PointerEventData data){
+		// reset everything
+		if (data.pointerId == pointerID) {
+			touched = false;
+			direction = Vector2.zero;
+		}
+	}
+
+	public Vector2 GetDirection(){
+		smoothDirection = Vector2.MoveTowards (smoothDirection, direction, smoothing);
+		return smoothDirection;
+	}
+
+}
